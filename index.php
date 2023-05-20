64b505760e42f1ce79c514f23ca9cf5bda84582c
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-20 10:37:03 +0900

    Milestone 1: ok

--- index.php
+++ index.php
@@ -40,9 +40,28 @@
 
     ];
 
-    foreach($hotels as $associative_array)
+    $numeral_array  = [
+        'Primo',
+        'Secondo',
+        'Terzo',
+        'Quarto',
+        'Quinto'
+    ];
+
+    foreach($hotels as $index => $associative_array)
     {
-        
+        echo "<br";
+        echo $numeral_array[$index] . " hotel della lista: <br>";
+        echo "- " . $associative_array['name'] . "<br>";
+        echo "Voto: " . $associative_array['vote'] . "<br>";
+        echo "Distanza dal centro: " . $associative_array['distance_to_center'] . "Km<br>";
+        $parking = "Presente";
+        if (! $associative_array['parking'])
+        {
+            $parking = "Assente";
+        }
+        echo "Parcheggio: " . $parking . "<br>";
+        echo "----------------------------------------<br>";
     }
 
 ?>

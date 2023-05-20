459589ef52b157069f4716915ee3a049f318d5c1
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-20 11:54:28 +0900

    Ciclo for di preparazione thead: ok

--- index.php
+++ index.php
@@ -40,28 +40,65 @@
 
     ];
 
-    $numeral_array  = [
-        'Primo',
-        'Secondo',
-        'Terzo',
-        'Quarto',
-        'Quinto'
-    ];
+    // Array di numeri ordinali per elencare gli hotel nella Milestone 1
+    // $numeral_array  = [
+    //     'Primo',
+    //     'Secondo',
+    //     'Terzo',
+    //     'Quarto',
+    //     'Quinto'
+    // ];
 
-    foreach($hotels as $index => $associative_array)
-    {
-        echo "<br";
-        echo $numeral_array[$index] . " hotel della lista: <br>";
-        echo "- " . $associative_array['name'] . "<br>";
-        echo "Voto: " . $associative_array['vote'] . "<br>";
-        echo "Distanza dal centro: " . $associative_array['distance_to_center'] . "Km<br>";
-        $parking = "Presente";
-        if (! $associative_array['parking'])
-        {
-            $parking = "Assente";
-        }
-        echo "Parcheggio: " . $parking . "<br>";
-        echo "----------------------------------------<br>";
-    }
+    // Output della Milestone 1
+    // foreach($hotels as $index => $associative_array)
+    // {
+    //     echo "<br";
+    //     echo $numeral_array[$index] . " hotel della lista: <br>";
+    //     echo "- " . $associative_array['name'] . "<br>";
+    //     echo "Voto: " . $associative_array['vote'] . "<br>";
+    //     echo "Distanza dal centro: " . $associative_array['distance_to_center'] . "Km<br>";
+    //     $parking = "Presente";
+    //     if (! $associative_array['parking'])
+    //     {
+    //         $parking = "Assente";
+    //     }
+    //     echo "Parcheggio: " . $parking . "<br>";
+    //     echo "----------------------------------------<br>";
+    // }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link a Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Hotel - PHP</title>
+</head>
+<body>
+    <header>
+        <h1 class="text-center text-primary">Hotel - PHP</h1>
+    </header>
+    <main>
+        <table class="table">
+            <thead>
+                <tr>
+                    <?php
+                        $keys = array_keys($hotels[0]);
+                        var_dump($keys);
+                        var_dump(count($keys));
+                        for ($i = 0; $i < count($keys); $i++)
+                            if ($keys[$i] != "description")
+                                echo $keys[$i] . "<br>";
+                            else
+                                echo "Trovato il non voluto: " . $keys[$i] . "<br>";
+                    ?> 
+                </tr>
+            </thead>
+        </table>
+    </main>
+</body>
+</html>
